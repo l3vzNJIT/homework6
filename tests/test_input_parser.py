98e1d5746f28d549b41db461c435ae6ae6ee7bb7
Lev <lzelenin@njit.edu>
2025-03-11 11:16:15 -0400

    Added a test using pytest fixture
    Signed-off-by: Lev <lzelenin@njit.edu>

--- tests/test_input_parser.py
+++ tests/test_input_parser.py
@@ -1,6 +1,7 @@
 """ Tests for the input string parser
 """
 
+import pytest
 from app.input_parser import CommandInput
 
 def test_parser():
@@ -12,3 +13,27 @@ def test_parser():
     assert parsed_command1.num_args == 2
     assert parsed_command1.args["argument_1"] == "1"
     assert parsed_command1.args["argument_2"] == "1"
+
+@pytest.fixture
+def example_command():
+    """ Define a pytest fixture for a single command """
+    return {
+            "input_str": "command arg1 arg2 arg3",
+            "command": "command",
+            "num_args" : 3,
+            "args" : {
+                     "argument_1": "arg1",
+                     "argument_2": "arg2",
+                     "argument_3": "arg3"
+                     }
+            }
+
+def test_with_fixture(example_command):  # pylint: disable=redefined-outer-name
+    """ Test the CommandInput parser with a fixture """
+    parsed_command1 = CommandInput(example_command["input_str"])
+    assert parsed_command1.input_string == example_command["input_str"]
+    assert parsed_command1.command == example_command["command"]
+    assert parsed_command1.num_args == example_command["num_args"]
+    assert parsed_command1.args["argument_1"] == example_command["args"]["argument_1"]
+    assert parsed_command1.args["argument_2"] == example_command["args"]["argument_2"]
+    assert parsed_command1.args["argument_3"] == example_command["args"]["argument_3"]
