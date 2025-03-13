6f2382e27e958c9801be72ffcce6ee9cb57bf639
Lev <lzelenin@njit.edu>
2025-03-13 12:12:37 -0400

    Added a batch of 100 tests using pytest.parametrize
    Signed-off-by: Lev <lzelenin@njit.edu>

--- tests/test_input_parser.py
+++ tests/test_input_parser.py
@@ -43,8 +43,7 @@ def test_with_fixture(example_command):  # pylint: disable=redefined-outer-name
     assert parsed_command.args["argument_3"] == example_command["args"]["argument_3"]
 
 
-@pytest.fixture
-def ex_rnd_cmd():
+def gen_rnd_cmd():
     """ Generate a random command string with Faker """
     fake = Faker()
     fake_command = {"command": fake.word(), "num_args": fake.random_digit()}
@@ -57,6 +56,12 @@ def ex_rnd_cmd():
     return fake_command
 
 
+@pytest.fixture
+def ex_rnd_cmd():
+    """ Wrap a random command generation into a fixtrue """
+    return gen_rnd_cmd()
+
+
 def test_with_random_fixture(ex_rnd_cmd):  # pylint: disable=redefined-outer-name
     """ Test the CommandInput parser with a randomized fixture """
     parsed_command = CommandInput(ex_rnd_cmd["input_str"])
@@ -65,3 +70,19 @@ def test_with_random_fixture(ex_rnd_cmd):  # pylint: disable=redefined-outer-nam
     assert parsed_command.num_args == ex_rnd_cmd["num_args"]
     for i in range(1, ex_rnd_cmd["num_args"] + 1):
         assert parsed_command.args[f"argument_{i}"] == ex_rnd_cmd["args"][f"argument_{i}"]
+
+
+# set up 100 test cases
+batch_of_tests = []
+for _ in range(100):
+    batch_of_tests.append(gen_rnd_cmd())
+@pytest.mark.parametrize("command_input", batch_of_tests)
+
+def test_with_random_commands(command_input):
+    """ Test the CommandInput parser with a randomized fixture """
+    parsed_command = CommandInput(command_input["input_str"])
+    assert parsed_command.input_string == command_input["input_str"]
+    assert parsed_command.command == command_input["command"]
+    assert parsed_command.num_args == command_input["num_args"]
+    for i in range(1, command_input["num_args"] + 1):
+        assert parsed_command.args[f"argument_{i}"] == command_input["args"][f"argument_{i}"]
