534583e061629278677fa0633087dc87ff91e23e
Lev <lzelenin@njit.edu>
2025-03-13 11:41:51 -0400

    Adding faker to tests for randomized input
    Signed-off-by: Lev <lzelenin@njit.edu>

--- tests/test_input_parser.py
+++ tests/test_input_parser.py
@@ -2,17 +2,20 @@
 """
 
 import pytest
+from faker import Faker
 from app.input_parser import CommandInput
+
 
 def test_parser():
     """ Test a command with two arguments """
     command1 = "add 1 1"
-    parsed_command1 = CommandInput(command1)
-    assert parsed_command1.input_string == command1
-    assert parsed_command1.command == "add"
-    assert parsed_command1.num_args == 2
-    assert parsed_command1.args["argument_1"] == "1"
-    assert parsed_command1.args["argument_2"] == "1"
+    parsed_command = CommandInput(command1)
+    assert parsed_command.input_string == command1
+    assert parsed_command.command == "add"
+    assert parsed_command.num_args == 2
+    assert parsed_command.args["argument_1"] == "1"
+    assert parsed_command.args["argument_2"] == "1"
+
 
 @pytest.fixture
 def example_command():
@@ -28,12 +31,37 @@ def example_command():
                      }
             }
 
+
 def test_with_fixture(example_command):  # pylint: disable=redefined-outer-name
     """ Test the CommandInput parser with a fixture """
-    parsed_command1 = CommandInput(example_command["input_str"])
-    assert parsed_command1.input_string == example_command["input_str"]
-    assert parsed_command1.command == example_command["command"]
-    assert parsed_command1.num_args == example_command["num_args"]
-    assert parsed_command1.args["argument_1"] == example_command["args"]["argument_1"]
-    assert parsed_command1.args["argument_2"] == example_command["args"]["argument_2"]
-    assert parsed_command1.args["argument_3"] == example_command["args"]["argument_3"]
+    parsed_command = CommandInput(example_command["input_str"])
+    assert parsed_command.input_string == example_command["input_str"]
+    assert parsed_command.command == example_command["command"]
+    assert parsed_command.num_args == example_command["num_args"]
+    assert parsed_command.args["argument_1"] == example_command["args"]["argument_1"]
+    assert parsed_command.args["argument_2"] == example_command["args"]["argument_2"]
+    assert parsed_command.args["argument_3"] == example_command["args"]["argument_3"]
+
+
+@pytest.fixture
+def ex_rnd_cmd():
+    """ Generate a random command string with Faker """
+    fake = Faker()
+    fake_command = {"command": fake.word(), "num_args": fake.random_digit()}
+    args = []
+    fake_command["args"] = {}
+    for i in range(1, fake_command["num_args"] + 1):
+        fake_command["args"][f"argument_{i}"] = fake.word()
+        args.append(fake_command["args"][f"argument_{i}"])
+    fake_command["input_str"] = " ".join([fake_command["command"]] + args)
+    return fake_command
+
+
+def test_with_random_fixture(ex_rnd_cmd):  # pylint: disable=redefined-outer-name
+    """ Test the CommandInput parser with a randomized fixture """
+    parsed_command = CommandInput(ex_rnd_cmd["input_str"])
+    assert parsed_command.input_string == ex_rnd_cmd["input_str"]
+    assert parsed_command.command == ex_rnd_cmd["command"]
+    assert parsed_command.num_args == ex_rnd_cmd["num_args"]
+    for i in range(1, ex_rnd_cmd["num_args"] + 1):
+        assert parsed_command.args[f"argument_{i}"] == ex_rnd_cmd["args"][f"argument_{i}"]
