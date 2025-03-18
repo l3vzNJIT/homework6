e37fac0cc1f435aae48df7c21bdd0b27bfca05ba
Lev <lzelenin@njit.edu>
2025-03-18 15:41:22 -0400

    Added an exit - not a plugin as it doesn't return CommandOutput
    Signed-off-by: Lev <lzelenin@njit.edu>

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 """Main entry point to the app"""
 
+import re
 import importlib.metadata
 from app.parser import CommandInput
 
@@ -10,19 +11,27 @@ for entry_point in importlib.metadata.entry_points(group="calculator.plugins"):
 
 print(f"Plugins loaded: {plugins}")
 
+exit_str = re.compile(r"^\s*(exit|quit)", re.IGNORECASE)
+
 while True:
     try:
         cmd = CommandInput(input(">>> "))
+
+        if exit_str.match(cmd.command):
+            break
+
         plg = []
         for plugin in plugins.values():
             if plugin.in_scope(cmd):
                 print(f">>> Plugin {plugin} in scope")
                 plg.append(plugin)
+
         if len(plg) == 0:
             print(f">>> Unrecognized command: {cmd.command}")
         elif len(plg) > 1:
             print(f">>> Amibiguous command: {cmd.command}")
         else:
             print(f">>> {plg[0](cmd).execute()}")
+
     except (EOFError, KeyboardInterrupt):
         break
